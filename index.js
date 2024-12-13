0e7c736aa03b49856edd3271cc7328797c924296
Nabin Poudel <nabinpoudel788@gmail.com>
2024-12-13 17:53:18 +0545

    create a connection and disconnection between client and server

--- index.js
+++ index.js
@@ -7,22 +7,20 @@ const app = express();
 const server = http.createServer(app);
 const io = socket(server);
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 
+// Serve static files from the 'public' directory
 const staticFile = path.join(__dirname, "./public");
-
 app.use(express.static(staticFile));
-
-app.get("/", (req,res) => {
-    res.send("index");
-});
 
 io.on("connection", (socket) => {
     console.log("User connected to server");
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected from server");
+    });
 });
 
-
 server.listen(port, () => {
-    console.log(`http://localhost:${port}`);
-    
-})
+    console.log(`Server is running at http://localhost:${port}`);
+});
